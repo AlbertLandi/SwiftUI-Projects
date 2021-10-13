b906eabda80dfe8a107d24eb5a67362bd4ede260
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2021-10-13 15:22:36 -0400

    Added files belonging to the WeSplit source code

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alberto Landi Cortiñas on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
