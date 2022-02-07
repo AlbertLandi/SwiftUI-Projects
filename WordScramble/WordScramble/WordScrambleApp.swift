e41e607b27a43568304fee18772bfa96379beeab
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-02-07 16:09:28 -0400

    Added WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alberto Landi Cortiñas on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
