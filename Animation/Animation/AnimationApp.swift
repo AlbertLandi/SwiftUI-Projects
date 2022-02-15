b26bcec95e9e216853ad228845d41a18e00720c6
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-02-15 12:48:54 -0400

    Added Animation

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Alberto Landi Cortiñas on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
