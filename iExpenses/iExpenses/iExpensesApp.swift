3a3055cfa1bc046e7da57e0ad35011575f95b760
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-02-19 14:24:11 -0400

    Added iExpenses (as actual project)

---
+++ iExpenses/iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Alberto Landi Cortiñas on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
