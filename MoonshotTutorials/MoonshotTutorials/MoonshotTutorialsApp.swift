768d3caadbb216650d71a1cbe7635a64f47add18
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-02-23 15:56:08 -0400

    Added MoonshotTutorials

---
+++ MoonshotTutorials/MoonshotTutorials/MoonshotTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTutorialsApp.swift
+//  MoonshotTutorials
+//
+//  Created by Alberto Landi Cortiñas on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
