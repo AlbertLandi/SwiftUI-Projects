b92f3efbe10419d97e09f06d6834d226ff66568a
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-02-25 15:34:31 -0400

    Added Moonshot

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Alberto Landi Cortiñas on 2/23/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
