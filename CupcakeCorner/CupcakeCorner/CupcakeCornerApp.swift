4f6afaa7534bbb508da63c3372e643b000e560f1
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-03-12 10:50:56 -0400

    Added Cupcake Corner (Project)

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alberto Landi Cortiñas on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
