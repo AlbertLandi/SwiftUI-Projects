e69d8835890762f4a9695ff0ad4da8692b6e2086
Alberto Landi Cortiñas <37026303+AlbertLandi@users.noreply.github.com>
2022-05-11 15:21:48 -0400

    Added Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Alberto Landi Cortiñas on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
